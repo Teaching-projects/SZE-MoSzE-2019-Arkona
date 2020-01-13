a0e2fe18af9885f7553d5a3f7acc4f9a43212b8e
Benyó Ádám <44004785+phenyo97@users.noreply.github.com>
2020-01-13 18:17:47 +0100

    doxy comments

--- filesystem.h
+++ filesystem.h
@@ -15,27 +15,84 @@ private:
     vector<Directory*> currentLocation;
 
     void printUserandLocation();
+        
+    /**
+    * 
+    */
+    
     void runCommand(string line);
+        
+    /**
+    * 
+    */
+    
     void mkdir(stringstream& ss);
+        
+    /**
+    * @brief The mkdir() shall create a new directory.
+    */
+    
     void touch(stringstream& ss);
-
+        
+    /**
+    * @brief List of directories/files.
+    */
+    
     void ls(stringstream& ss);
-
+        
+    /**
+    * @brief Command that lists directory contents of files and other directories.
+    */
+    
 
     void echo(stringstream& ss);
+            
+    /**
+    * @brief Moves the elements from one folder to another.
+    */
+    
     void trimNames(char character);
 
     std::vector<Directory *> parseRelativePath(string arg);
     Directory* getRelativeDir(string path);
 
     void cd(stringstream& ss);
+            
+    /**
+    * @brief Moves into a folder in the tree.
+    */
+    
     void cdRelativePath(string arg);
-
+            
+    /**
+    * @brief Relative path is defined as path related tothe present working directory.
+    */
+    
     void rm(stringstream& ss);
+            
+    /**
+    * @brief Command to delete a file or directory.
+    */
+    
     void deleteDirFor(string name, Directory *location);
+            
+    /**
+    * 
+    */
+    
 
     void startup(string* filename);
+            
+    /**
+    * 
+    */
+    
     void exit(string* filename);
+            
+    /**
+    * @brief To exit the sehll where it is currently running.
+    */
+    
 public:
     Filesystem(string* filename){
         startup(filename);
