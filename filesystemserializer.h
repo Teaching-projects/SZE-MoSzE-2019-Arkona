bfcf9f233f9a47bf5a7bc3c6181ba50c5394b1d9
Benyó Ádám <44004785+phenyo97@users.noreply.github.com>
2020-01-13 18:00:34 +0100

    doxy comments

--- filesystemserializer.h
+++ filesystemserializer.h
@@ -11,8 +11,17 @@ public:
     FileSystemSerializer();
 
     static std::string encode(Directory* root);
+        
+    /**
+    * This option is used to encode any data from standard input or from any file.
+    */
+    
     static Directory* decode(std::string json);
-
+        
+    /**
+    * This option is used to decode any encoded data from standard input or from any file.
+    */
+    
 };
 
 #endif // FILESYSTEMSERIALIZER_H
