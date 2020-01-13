9fadb66ba59601c3d05c5f1be0ec3eba46d6f374
Benyó Ádám <44004785+phenyo97@users.noreply.github.com>
2020-01-13 17:49:53 +0100

    doxy comments

--- directory.h
+++ directory.h
@@ -32,22 +32,63 @@ public:
 
     int mkdir(string dirName);
     int mkdir(Directory* dir);
+    
+    /**
+    * The mkdir() shall create a new directory.
+    */
 
     int touch(string fileName, string content);
     int touch(File* file);
+    
+    /**
+    * List of directories/files.
+    */
 
     bool isEmpty() const;
 
     std::string getJsonContent(std::string);
 
     void deleteDirectory(const string& d);
+    
+    /**
+    * Command to delete a directory.
+    */
+    
     void deleteFile(const string& fileName);
+        
+    /**
+    * Command to delete a file.
+    */
+    
     void rm();
+        
+    /**
+    * Command to delete a file or directory.
+    */
+    
     void treelist(int indent) const;
+        
+    /**
+    * List contents of directories and files in a treelike format.
+    */
+    
     void ls() const;
-
+        
+    /**
+    * Command that lists directory contents of files and other directories.
+    */
+    
     bool containsFile(const string& fileName) const;
+        
+    /**
+    * Checcks a directory if it contains files.
+    */
+    
     bool containsDirectory(const string& dirName) const;
+        
+    /**
+    * Checks a directory if it contains directories.
+    */
 
     Directory* getDirectory(const string& dirname) const;
     File* getFile(const string& fileName) const;
